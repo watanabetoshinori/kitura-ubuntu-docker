95e3c4871f77033d25b8d9df36037cbba2dbb68f
rolivieri <roliv@us.ibm.com>
2016-02-24 22:08:04 -0600

    Updates to ci.sh script.

--- ci.sh
+++ ci.sh
@@ -22,8 +22,6 @@
 # If any commands fail, we want the shell script to exit immediately.
 set -e
 
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-
 # Clone and build Kitura
 ./build_kitura.sh
 
